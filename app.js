10ada50432f68f33efd41d03ca12ee3c86426d80
Heidi Fryzell <hfryzell@gmail.com>
2021-06-24 15:43:53 -0400

    everything working but maxScore

--- app.js
+++ app.js
@@ -1,1 +1,52 @@
-alert('hello');
+let playerOneScore = 0;
+let playerTwoScore = 0;
+
+
+
+const playerOneDisplay = document.querySelector('#playerOneDisplay');
+const playerTwoDisplay = document.querySelector('#playerTwoDisplay');
+const scoreSelect = document.querySelector('select');
+const buttonOne = document.querySelector('#playerOneButton');
+const buttonTwo = document.querySelector('#playerTwoButton');
+const buttonReset = document.querySelector('#resetButton');
+
+scoreSelect.addEventListener('input', function () {
+    let maxScore = scoreSelect.value;
+    console.log(maxScore);
+    return maxScore;
+});
+
+buttonOne.addEventListener('click', function (e) {
+    playerOneScore += 1;
+    playerOneDisplay.innerText = `${playerOneScore}`;
+    console.log(maxScore);
+    if (playerOneScore === maxScore) {
+        playerOneDisplay.style.color = "green";
+        buttonOne.disabled = true;
+        playerTwoDisplay.style.color = "red";
+        buttonTwo.disabled = true;
+    }
+    e.stopPropagation();
+});
+
+buttonTwo.addEventListener('click', function () {
+    playerTwoScore += 1;
+    playerTwoDisplay.innerText = `${playerTwoScore}`;
+    console.log(maxScore);
+    if (playerTwoScore === maxScore) {
+        playerTwoDisplay.style.color = "green";
+        buttonTwo.disabled = true;
+        playerOneDisplay.style.color = "red";
+        buttonOne.disabled = true;
+    }
+    e.stopPropagation();
+});
+
+buttonReset.addEventListener('click', function () {
+    buttonTwo.disabled = false;
+    buttonOne.disabled = false;
+    playerOneDisplay.innerText = `0`;
+    playerTwoDisplay.innerText = `0`;
+    playerOneDisplay.style.color = "black";
+    playerTwoDisplay.style.color = "black";
+});
