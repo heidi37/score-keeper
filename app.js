0f7664686f9871e015ac6ebee41f2db3509aaf72
Heidi Fryzell <hfryzell@gmail.com>
2021-06-25 12:54:17 -0400

    styled with Bulma

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ let playerOneScore = 0;
 let playerTwoScore = 0;
 let maxScore = 5;
 
-
 const playerOneDisplay = document.querySelector('#playerOneDisplay');
 const playerTwoDisplay = document.querySelector('#playerTwoDisplay');
-const scoreSelect = document.querySelector('select');
+const scoreSelect = document.querySelector('#playingto');
 const buttonOne = document.querySelector('#playerOneButton');
 const buttonTwo = document.querySelector('#playerTwoButton');
 const buttonReset = document.querySelector('#resetButton');
 
 scoreSelect.addEventListener('input', function () {
-    maxScore = parseInt(scoreSelect.value);
+    maxScore = parseInt(this.value);
+    scoreSelect.disabled = true;
     console.log(maxScore);
     return maxScore;
 });
@@ -21,9 +21,9 @@ buttonOne.addEventListener('click', function (e) {
     playerOneDisplay.innerText = `${playerOneScore}`;
     console.log(maxScore);
     if (playerOneScore === maxScore) {
-        playerOneDisplay.style.color = "green";
+        playerOneDisplay.classList.add('has-text-success');
         buttonOne.disabled = true;
-        playerTwoDisplay.style.color = "red";
+        playerTwoDisplay.classList.add('has-text-danger');
         buttonTwo.disabled = true;
     }
 });
@@ -33,9 +33,9 @@ buttonTwo.addEventListener('click', function (e) {
     playerTwoDisplay.innerText = `${playerTwoScore}`;
     console.log(maxScore);
     if (playerTwoScore === maxScore) {
-        playerTwoDisplay.style.color = "green";
+        playerTwoDisplay.classList.add('has-text-success');
         buttonTwo.disabled = true;
-        playerOneDisplay.style.color = "red";
+        playerOneDisplay.classList.add('has-text-danger');
         buttonOne.disabled = true;
     }
 });
@@ -43,8 +43,13 @@ buttonTwo.addEventListener('click', function (e) {
 buttonReset.addEventListener('click', function () {
     buttonTwo.disabled = false;
     buttonOne.disabled = false;
+    playerOneScore = 0;
+    playerTwoScore = 0;
     playerOneDisplay.innerText = `0`;
     playerTwoDisplay.innerText = `0`;
-    playerOneDisplay.style.color = "black";
-    playerTwoDisplay.style.color = "black";
+    playerOneDisplay.classList.remove('has-text-success', 'has-text-danger')
+    playerTwoDisplay.classList.remove('has-text-success', 'has-text-danger')
+    scoreSelect.disabled = false;
+    scoreSelect.value = 5;
+    maxScore = 5;
 });
